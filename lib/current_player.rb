5b14cf114d207bc3207b2a73c166ba0cd6de8bbc
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-05 13:05:21 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  counter = 0
+  board.each do |square|
+    if square == "X" || square == "O"
+      counter += 1
+    else end
+    end
+    return counter
+  end
+
+      
+  
